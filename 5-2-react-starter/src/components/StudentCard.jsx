26462b1c9e8cf9f26fd3ee113844e3ebb8894097
Your GitHub Name <lujain.aljehani@outlook.com>
2025-10-01 11:26:11 +0300

    Added StudentCard

---
+++ 5-2-react-starter/src/components/StudentCard.jsx
@@ -0,0 +1,11 @@
+function StudentCard() {
+    return (
+        <div className="student-card">
+            <h3>Lujain</h3>
+            <p>ID: 202249140</p>
+            <p>Department: Information and Computer Science</p>
+        </div>
+    );
+}
+
+export default StudentCard;
