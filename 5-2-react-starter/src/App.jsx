538d27d2d622f11147fc7b95ed0052eef1a77234
Your GitHub Name <lujain.aljehani@outlook.com>
2025-10-01 11:29:12 +0300

    updated App

--- 5-2-react-starter/src/App.jsx
+++ 5-2-react-starter/src/App.jsx
@@ -1,20 +1,23 @@
+
 import './App.css'
+import StudentCard from './components/StudentCard'
 
 function App() {
-  return (
-    <div className="app">
-      <header className="dashboard-header">
-        <h1>Student Information Dashboard</h1>
-        <p>View and manage student details</p>
-      </header>
+    return (
+        <div className="app">
+            <header className="dashboard-header">
+                <h1>Student Information Dashboard</h1>
+                <p>View and manage student details</p>
+            </header>
 
-      <main className="dashboard-main">
-        <div className="cards-container">
-          {/* TODO: Import and render StudentCard components here */}
+            <main className="dashboard-main">
+                <div className="cards-container">
+                    <StudentCard></StudentCard>
+                </div>
+            </main>
         </div>
-      </main>
-    </div>
-  )
+    )
 }
 
 export default App
+
